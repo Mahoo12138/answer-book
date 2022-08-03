007c2762eb326276850bd34279d384f0eda99305
Mahoo12138 <mahoo12138@outlook.com>
2022-08-03 16:02:21 +0800

    :new: Add Q&A option

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Context, Time, segment, Schema } from 'koishi'
 
-import * as Answer from './answer.json'  
+import * as Answer from './answer.json'
+
+import { randomIdFormQuestion } from './utils'
 
 export const name = 'answerbook'
 
 
 interface Answer {
-  [id: number] : {
-    answer: string
-  }
+  answer: string
 }
 
 interface Config {
@@ -19,16 +19,27 @@ export const Config = Schema.object({
   duration: Schema.number().default(10).min(1).max(20).description('答案的等待时长'),
 })
 
-const answer = Answer as Answer
+const answerList = Answer as Answer[]
 
 export function apply(ctx: Context, cfg: Config) {
-  ctx.command('answer', '翻阅答案之书')
-    .example('answer')
-    .example('翻阅答案')
-    .shortcut('翻阅答案')
+  ctx.command('answer', '在答案之书中寻找答案')
+    .example('answer -q 今天我要摸鱼吗？')
+    .example('翻阅答案/答案之书')
+    .option('ask', '-a 采用问答模式')
+    .option('question', '-q <question:string> 设定你想询问的问题')
+    .shortcut('翻阅答案', { options: {} })
+    .shortcut('答案之书', { options: { ask: true } })
     .action(async ({ options, session }) => {
-      await session.sendQueued('在心中默念你的问题，之后答案之书会给你答案', cfg.duration * Time.second)
-      const id = Math.floor(Math.random() * 286)
-      await session.sendQueued(segment('at', { id: session.userId }) + `${answer[id].answer}`)
+      console.log(options)
+      let { question } = options;
+      if (options.ask && !options.question) {
+        session.send("有什么困扰你的难题呢？")
+        question = await session.prompt(30 * 1000)
+      } else if(!options.question){
+        await session.sendQueued('在心中默念你的问题，之后答案之书会给你答案', cfg.duration * Time.second)
+        question = session.username + new Date().toISOString();
+      }
+      const id = randomIdFormQuestion(question, answerList?.length)
+      await session.sendQueued(segment('at', { id: session.userId }) + `${answerList[id].answer}`)
     })
 }
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+const randomIdFormQuestion = (question: string, max: number = 1, min = 0) => {
+  let seed = string2Hash(question);
+  seed = (seed * 9301 + 49297) % 233280;
+  const rnd = seed / 233280.0;
+  return Math.ceil(min + rnd * (max - min));
+}
+
+const string2Hash = (str: string) => {
+  let hash = 0, i = 0, chr = 0;
+  if (str.length === 0) return hash;
+  for (; i < str.length; i++) {
+    chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0;
+  }
+  return hash;
+};
+
+export {randomIdFormQuestion}
